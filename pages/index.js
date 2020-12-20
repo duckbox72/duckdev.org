c5dbd7b65e9ffc2ff8331782ff0661bf227d5ed4
Luis Felipe Klaus <duckbox@mac-dev.local>
2020-12-20 14:15:36 -0300

    first page

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+function Home() {
+    return (
+    <div>
+        <h1>Home</h1>
+        <Counter />
+    </div>
+    )
+}
+
+function Counter() {
+    const [counter,setCounter] = useState(1);
+
+    function addCounter() {
+        setCounter(counter + 1);
+    }
+    return (
+        <div>
+            
+            <button onClick={addCounter}>Add</button>
+        </div>
+    )
+}
+export default Home
