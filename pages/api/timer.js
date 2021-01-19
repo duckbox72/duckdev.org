3bef04802aa6e29f4ab2ec709981075a57aee8f3
duckbox72 <duckbox@hotmail.com>
2021-01-19 20:58:41 -0300

    cache control added to header fetch commented

--- pages/api/timer.js
+++ pages/api/timer.js
@@ -2,10 +2,11 @@ async function timer(request, response) {
     const apiSecret = process.env.CONVERTKIT_API_SECRET;
     const dynamicDate = new Date();
 
-    const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
-    const subscribersResponseJson = await subscribersResponse.json();
-    const inscritos = subscribersResponseJson.total_subscribers;
+    //const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`);
+    //const subscribersResponseJson = await subscribersResponse.json();
+    const inscritos = 10000 //subscribersResponseJson.total_subscribers;
 
+    // auto revalidate every 10 seconds, return cache while revalidate
     response.setHeader('Cache-control', 's-maxage=10, stale-while-revalidate');
     
     response.json({
