5c1180fe0b5a659f5d5ca7e6954918d0542da940
Luis Felipe Klaus <duckbox@hotmail.com>
2021-10-28 17:00:11 -0300

    header icon handled:

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
+import { GiPlasticDuck } from "react-icons/gi";
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Title } from './HeaderStyles';
 
@@ -10,7 +11,10 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{display: 'flex', alignItems: 'center', 'color': 'white', marginBottom: '20px'}}>
-          <DiCssdeck size='3rem' /> <Title>Portfolio</Title>
+          <GiPlasticDuck 
+           size='3rem' 
+           style={{marginRight: 4}}
+          /> <Title>duckdev.org</Title>
         </a>
       </Link>
     </Div1>
