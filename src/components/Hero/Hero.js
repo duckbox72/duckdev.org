e977660ff47f8eca29c657fe898177ad403f8240
Luis Felipe Klaus <duckbox@hotmail.com>
2021-10-28 15:55:44 -0300

    avatar moved inside section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,12 +7,12 @@ import { Avatar, AvatarContainer, LeftSection } from './HeroStyles';
 const Hero = () => (
   <Section row nopadding>
     <LeftSection>
-      {/* <AvatarContainer>
-        <Avatar src='/images/face_stamp.jpg'/>
-      </AvatarContainer> */}
+        <AvatarContainer>
+          <Avatar src='/images/face_stamp.jpg'/>
+        </AvatarContainer> <br />
       <SectionTitle main center>
         Luis Felipe Klaus <br />
-        Full Stack Developer 
+        <span>Full Stack Developer</span> 
       </SectionTitle>
       <SectionText>
         Hi, i have created this portfolio to  easily showcase and share from a single place my personal information, projects, skills, accomplishments and contact.  
