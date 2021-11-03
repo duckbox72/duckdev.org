0e824d96550b4096d6e7fe933a74469eef67807d
Luis Felipe Klaus <duckbox@hotmail.com>
2021-11-03 17:49:26 -0300

    CardInfo style update

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -79,7 +79,7 @@ export const CardInfo = styled.p`
   line-height: 20px;
   text-align: justify;
   @media ${(props) => props.theme.breakpoints.sm} {
-    padding: 3rem
+    padding: 1.5rem;
   
 }
 `;
