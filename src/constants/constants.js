0f74bb1b9e12345e3cf3a9ebe47bd498ee9aa64c
Luis Felipe Klaus <duckbox@hotmail.com>
2021-11-07 19:49:48 -0300

    constants update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -58,9 +58,9 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 'Mar 20', text: 'Started my journey', },
+  { year: 'Jun 20', text: 'Finished IBM Data Science'},
+  { year: 'Nov 20', text: 'Finished CS50' },
+  { year: 'Jun 21', text: 'Awarded Profesional Certificate' },
+  { year: 'Oct 21', text: 'Awarded Profesional Certificate'},
 ];
