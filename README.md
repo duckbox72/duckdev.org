8be103a2fb63abf53b0a9882f02c3bd257667b73
Luis Felipe Klaus <duckbox@hotmail.com>
2021-12-11 18:00:54 -0300

    Hero text done3

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 Self-driven passionate developer, commited to detail, who loves to continually learn and to create intelligent meanigful solutions to real world problems. 
 
-Solid experience working with a number of technologies in the areas of web development and data-science, covering from several languages, architectures, frameworks and database solutions to the most popular data analisys and visualization libraries, as well as a number of oriented to AI and machine learning libraries. 
+Solid experience working with a number of technologies in development and data-science, covering from several languages, architectures, frameworks and database solutions to the most popular data analisys and visualization libraries, as well as a number of oriented to AI and machine learning libraries. 
 
 In that spirit i'm very excited to contribute with something that really impacts on people's lives.
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,7 +17,7 @@ const Hero = () => (
       </SectionTitle>
       <SectionText>
       {"Self-driven passionate developer, commited to detail, who loves to continuously learn and to create intelligent meanigful solutions to real world problems. "}   
-      {"Solid experience working with a number of technologies in the areas of web development and data-science, covering from several languages, architectures, frameworks and database solutions to the most popular data analisys and visualization libraries, as well as a number of oriented to AI and machine learning libraries. "}
+      {"Solid experience working with a number of technologies in web development and data-science, covering from several languages, architectures, frameworks and database solutions to the most popular data analisys and visualization libraries, as well as a number of oriented to AI and machine learning libraries. "}
       {"In that spirit i'm very excited to contribute with something that really impacts on people's lives."}
       
       </SectionText>
