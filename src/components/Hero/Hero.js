4de431a325e8f67e74f64dc292a9e2c5d9efd013
Luis Felipe Klaus <duckbox@hotmail.com>
2021-12-11 19:38:45 -0300

    Hero fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,8 +17,8 @@ const Hero = () => (
       </SectionTitle>
       <SectionText>
       {"Self-driven passionate developer, commited to detail, who loves to continuously learn and to create intelligent meanigful solutions to real world problems. "}   
-      {"Solid experience working with a number of technologies in web development and data-science, covering from several languages, architectures, frameworks and database solutions to the most popular data analysis and visualization libraries, as well as a number of oriented to AI and machine learning libraries. "}
-      {"In that spirit i'm very excited to contribute with something that really impacts on people's lives."}
+      {"Solid experience working with a number of technologies in development and data-science, covering from several languages, architectures, frameworks and database solutions to the most popular data analysis and visualization libraries, as well as a number of AI and machine learning specific libraries. "}
+      {"In that spirit I'm very excited to contribute with something that really impacts on people's lives."}
       
       </SectionText>
       {/* <Button onClick={() => window.location='https://google.com'}>
