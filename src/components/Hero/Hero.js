2ee443535e4bfcbdb42e3d3a96e0a061b4134a57
Luis Felipe Klaus <duckbox@hotmail.com>
2021-12-11 17:08:05 -0300

    Hero text done2

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = () => (
         Full Stack Developer
       </SectionTitle>
       <SectionText>
-      {"Self-driven passionate developer, commited to detail, who loves to continually learn and to create intelligent meanigful solutions to real world problems. "}   
+      {"Self-driven passionate developer, commited to detail, who loves to continuously learn and to create intelligent meanigful solutions to real world problems. "}   
       {"Solid experience working with a number of technologies in the areas of web development and data-science, covering from several languages, architectures, frameworks and database solutions to the most popular data analisys and visualization libraries, as well as a number of oriented to AI and machine learning libraries. "}
       {"In that spirit i'm very excited to contribute with something that really impacts on people's lives."}
       
